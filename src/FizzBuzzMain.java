af3d40a9bf200121ab18675761b810bb99f10d58
tundrathewetland <dareenatj@gmail.com>
2024-09-09 10:36:27 -0400

    FizzBuzz

---
+++ src/FizzBuzzMain.java
@@ -0,0 +1,11 @@
+public class FizzBuzzMain {
+    public static void main(String[] args){
+        FizzBuzz.doFizzBuzz(1);
+        FizzBuzz.doFizzBuzz(2);
+        FizzBuzz.doFizzBuzz(3);
+        FizzBuzz.doFizzBuzz(4);
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.doFizzBuzz(15);
+        FizzBuzz.doFizzBuzz(17);
+    }
+}
